f19eedeafc713cb9537429c92e89932f0e72af41
enterpi-123 <b.mohanarao314@gmail.com>
2015-07-29 20:30:57 +0530

    File added file3 and file4

--- file3.php
+++ file3.php
@@ -6,4 +6,13 @@ function writeMsg() {
 }
 
 writeMsg();
+echo "<br>New changes made<br>";
+function setHeight($minheight = 50) {
+    echo "The height is : $minheight <br>";
+}
+
+setHeight(350);
+setHeight(); // will use the default value of 50
+setHeight(135);
+setHeight(80);
 ?>
--- file4.php
+++ file4.php
@@ -10,4 +10,13 @@ familyName("Hege");
 familyName("Stale");
 familyName("Kai Jim");
 familyName("Borge");
+echo "<br>New changes made<br>";
+function sum($x, $y) {
+    $z = $x + $y;
+    return $z;
+}
+
+echo "5 + 10 = " . sum(5, 10) . "<br>";
+echo "7 + 13 = " . sum(7, 13) . "<br>";
+echo "2 + 4 = " . sum(2, 4);
 ?>
