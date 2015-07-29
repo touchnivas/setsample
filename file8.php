a902be2e83ef0c5006c06e108bdc21468a27ab73
kasireddy4 <kasireddynivas@gmail.com>
2015-07-29 20:34:53 +0530

    removing file from commited file

--- file8.php
+++ file8.php
@@ -4,12 +4,4 @@ echo __FILE__;
 $str = addcslashes("Hello World!","W");
 echo($str); 
 
-
-
-<?php
-$number = 8;
-$str = "Beijing";
-$txt = sprintf("There are %u million bicycles in %s.",$number,$str);
-echo $txt;
-?>  
 ?>
--- file9.php
+++ file9.php
@@ -5,10 +5,5 @@ $str = "Hello World!";
 echo $str . "<br>";
 echo ltrim($str,"Hello");
 
-<?php
-$number = 9;
-$str = "Beijing";
-$txt = sprintf("There are %u million bicycles in %s.",$number,$str);
-
-?>  
+echo "nivas"; 
 ?>
