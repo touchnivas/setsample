15a81cfd287ec217449f8c8da431adfe230e2aa1
megireddy <megireddyece@gmail.com>
2015-07-29 20:37:35 +0530

    File added file1 and file2

--- file1.php
+++ file1.php
@@ -1,5 +1,5 @@
 <?php
 echo "FILE2:file1.php";
 echo "first git class";
-echo "files changed";
+echo "files ignore";
 ?>
--- file2.php
+++ file2.php
@@ -1,5 +1,5 @@
 <?php
 echo "changes of the code";
 echo "FILE2:file2.php";
-echo "files are changed";
+echo "git ignore";
 ?>
